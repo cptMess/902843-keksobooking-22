5248ba7aeb0d793edb15db935eac62c7cc26fbd4
cptMess <65182223+cptMess@users.noreply.github.com>
2021-02-15 19:51:13 +0300

    2. Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -4,11 +4,11 @@ const getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 getRandomInt(0, 101);
-console.log(getRandomInt(0, 101));
+//console.log(getRandomInt(0, 101));
 
 // случайное число с плавающей точкой
 const getRandomFloat = function (min, max, symbol) {
   return parseFloat((Math.random() * (max - min) + min).toFixed(symbol));
 }
 getRandomFloat(0, 1001, 1);
-console.log(getRandomFloat(0, 1001, 4));
+//console.log(getRandomFloat(0, 1001, 4));
